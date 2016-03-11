f4cbea879b6e1f530f104b852c31c1dec292e01f
Nicole Finnie <Nicole.Finnie@de.ibm.com>
2016-03-11 13:32:16 +0100

    added conditional playing alert

--- deviceScripts/client.py
+++ deviceScripts/client.py
@@ -6,6 +6,7 @@ import ibmiotf.application
 import uuid
 import quakeSensor
 import motionSensor
+import humitureSensor
 
 client = None
 
@@ -15,20 +16,30 @@ def myEarthquakeAlert(cmd):
         command = payload["command"]
         print command
         if command == "alert":
-            os.system('mpg321 -n 1000 music/imperialMarch.mp3')
+                if os.path.isfile('music/imperialMarch.mp3'):
+                        os.system('mpg321 -n 1000 music/imperialMarch.mp3')
+                else:
+                        print "Receive alerts from IoT, but no music file is found"
 
 try:
     options = ibmiotf.application.ParseConfigFile("/home/pi/device.cfg")
     options["deviceId"] = options["id"]
     options["id"] = "aaa" + options["id"]
     client = ibmiotf.application.Client(options)
+    print "try to connect to IoT"
     client.connect()
+    print "connect to IoT successfully"
     client.deviceEventCallback = myEarthquakeAlert
     client.subscribeToDeviceEvents(event="earthquake")
 
     quakeStatus = False
     motionStatus = False
+    humitureStatus = False
+
     while True:
+        humitureData = humitureSensor.sample()
+        jsonHumitureData = json.dumps(humitureData)
+        client.publishEvent("raspberrypi", options["deviceId"], "humitureSensor", "json", jsonHumitureData)
         quakeData = quakeSensor.sample()
         jsonQuakeData = json.dumps(quakeData)
         client.publishEvent("raspberrypi", options["deviceId"], "quakeSensor", "json", jsonQuakeData)
@@ -46,7 +57,6 @@ try:
             motionStatus = motionData['motionDetected']
             print "Change in motion detector status, motionDetected is now:", motionStatus
         time.sleep(0.5)
- 
+
 except ibmiotf.ConnectionException  as e:
     print e
-    
