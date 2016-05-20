22be9f59fc9e066f015992b2d503b56a231a890b
KALI <KALI@de.ibm.com>
2016-05-20 17:31:04 +0200

    fix rainSensor.py

--- deviceScripts/rainSensor.py
+++ deviceScripts/rainSensor.py
@@ -12,22 +12,23 @@ rainDelta = 2
 
 def setup(gpioPort, i2cAddress):
         global initialRainIntensityReading
+        global lowerBoundary
+        global upperBoundary
         GPIO.setmode(GPIO.BCM)
         ADC.setup(i2cAddress)
         GPIO.setup(gpioPort, GPIO.IN)
         initialRainIntensityReading = ADC.read(0)
         # set the delta zone so we dont need to recalc it every time
         lowerBoundary = initialRainIntensityReading - rainDelta
-        upperBoundary = initialRainIntensityReading - rainDelta
+        upperBoundary = initialRainIntensityReading + rainDelta
         print "initial rain intensity reading: ", initialRainIntensityReading
 
 def sample():
-        global initialRainIntensityReading
         currentRainIntensity = ADC.read(0)
         data = {}
         data['rainReading'] = currentRainIntensity
         # if any rain drop is detected
-        if (lowerBoundary <= currentRainIntensity <= upperBoundary) :
+        if (lowerBoundary > currentRainIntensity < upperBoundary) :
                 data['rainDetected'] = True;
         else:
                 data['rainDetected'] = False;
